25797d3224bb4d2b49ab0580ec9e777828a2544e
marcdhi <gandhi.mardav@gmail.com>
2024-01-11 05:55:32 +0530

    misc: testing connection

--- server/dist/managers/RoomManagers.js
+++ server/dist/managers/RoomManagers.js
@@ -7,18 +7,22 @@ class RoomManager {
         this.rooms = new Map();
     }
     createRoom(user1, user2) {
-        const roomId = this.generate();
+        const roomId = this.generate().toString();
+        console.log("this is rooom id:", roomId);
         this.rooms.set(roomId.toString(), {
             user1,
             user2
         });
+        console.log("created room");
+        console.log("here are the room", this.rooms);
         user1 === null || user1 === void 0 ? void 0 : user1.socket.emit("send-offer", {
             roomId
         });
     }
     onOffer(roomId, sdp) {
         var _a;
-        const user2 = (_a = this.rooms.get(roomId)) === null || _a === void 0 ? void 0 : _a.user1;
+        const user2 = (_a = this.rooms.get(roomId)) === null || _a === void 0 ? void 0 : _a.user2;
+        console.log("this is user2 ", user2);
         user2 === null || user2 === void 0 ? void 0 : user2.socket.emit("offer", {
             sdp //session description protocol
             ,
@@ -28,6 +32,7 @@ class RoomManager {
     onAnswer(roomId, sdp) {
         var _a;
         const user1 = (_a = this.rooms.get(roomId)) === null || _a === void 0 ? void 0 : _a.user1;
+        console.log("this is user1 ", user1);
         user1 === null || user1 === void 0 ? void 0 : user1.socket.emit("answer", {
             sdp,
             roomId
